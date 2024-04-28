e6a131e50c874634b93637b3ec65ad2326bb5c15
Nischal Shrestha <nischalnayaju.shrestha@gmail.com>
2024-04-28 22:58:27 +0545

    dev-kr : added layouts and links

---
+++ components/ui/dashboard/sidenav.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link';
+
+export default function SideNav() {
+    return (
+        <div className="flex h-full flex-col px-3 py-4 md:px-2">
+            <Link
+                className="mb-2 flex h-20 items-end justify-start rounded-md bg-[#2C75FF] p-4 md:h-40"
+                href="/"
+            >
+                <div className="w-32 text-white md:w-40">
+                    <p>Road Reporting</p>
+                </div>
+            </Link>
+            <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+                <Link
+
+                    href="/dashboard/reports"
+                    className="{clsx(
+                                'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                                {
+                                    'bg-sky-100 text-blue-600' : pathname === link.href
+                                }
+                                )}"
+                >
+                    <p className="hidden md:block">Reports</p>
+                </Link>
+
+                <Link
+
+                    href="/dashboard/customers"
+                    className="{clsx(
+                                'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                                {
+                                    'bg-sky-100 text-blue-600' : pathname === link.href
+                                }
+                                )}"
+                >
+                    <p className="hidden md:block">Users</p>
+                </Link>
+                <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+                <form>
+                    <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                        <div className="hidden md:block">Sign Out</div>
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
