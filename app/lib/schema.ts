6d6d7b601534dc34acf4bcffd2dc140a4b883943
AnantaPrajapati <anantaprajapati0@gmail.com>
2024-05-09 23:07:10 +0545

    report

---
+++ app/lib/schema.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const ReportSchema =  new mongoose.Schema({
+
+    userId: Object, 
+    location:String,
+    images: String,      
+    severity: String,
+    desc: String,
+    status:  String,  
+
+});
+
+export const Report = mongoose.models.reports || mongoose.model("reports", ReportSchema);
+
+
+
+
+
